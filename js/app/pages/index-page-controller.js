b3f0b5ace117a49a7924c923f7a67a85a187043c
Jorge Boullosa <jboullosa@gmail.com>
2015-08-28 00:27:39 -0300

    Alpha, Hour,Date Object

--- js/app/pages/index-page-controller.js
+++ js/app/pages/index-page-controller.js
@@ -1,5 +1,76 @@
 /*jslint browser: true*/
 /*global console*/
+var hoursPicker = function (input) {
+    'use strict';
+    return {
+        input: input,
+        rotateEffect: true,
+        formatValue: function (p, values, displayValues) {
+            return values[0] + ':' + values[1] + ' ' + values[2];
+        },
+        cols: [
+            {
+                values: ('00 01 02 03 04 05 06 07 08 09 10 11 12').split(' ')
+            },
+            {
+                values: (function () {
+                    var arr = [];
+                    for (var i = 0; i <= 59; i++) {
+                        arr.push(String('00' + i).slice(-2));
+                    }
+                    return arr;
+                })()
+            },
+            {
+                values: ['AM', 'PM']
+            }
+        ]
+    }
+}
+
+var datePicker = function (input) {
+    'use strict';
+    return {
+        input: input,
+        rotateEffect: true,
+
+        value: [today.getMonth(), today.getDate(), today.getFullYear()],
+
+        onChange: function (picker, values, displayValues) {
+            var daysInMonth = new Date(picker.value[2], picker.value[0]*1 + 1, 0).getDate();
+            if (values[1] > daysInMonth) {
+                picker.cols[1].setValue(daysInMonth);
+            }
+
+            selectedDate = new Date(values[2], values[0], values[1], 0, 0,0, 0)
+        },
+
+        formatValue: function (p, values, displayValues) {
+            return displayValues[0] + ' ' + values[1] + ', ' + values[2];
+        },
+
+        cols: [
+            // Months
+            {
+                values: ('0 1 2 3 4 5 6 7 8 9 10 11').split(' '),
+                displayValues: ('Enero Febrero Marzo Abril Mayo Junio Julio Agosto Septiembre Octubre Noviembre Diciembre').split(' '),
+                textAlign: 'left'
+            },
+            // Days
+            {
+                values: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
+            },
+            // Years
+            {
+                values: (function () {
+                    var arr = [];
+                    for (var i = 1950; i <= 2030; i++) { arr.push(i); }
+                    return arr;
+                })()
+            }
+        ]
+    }
+}
 
 var myapp = myapp || {};
 myapp.pages = myapp.pages || {};
